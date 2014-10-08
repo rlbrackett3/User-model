47380217994c9a574b9105642c9cf5c82472a90c
Neil Daftary <nmd278@nyu.edu>
2014-10-08 17:27:17 -0400

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	
+	def index
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+		flash[:notice] = "Welcome to the site!"
+		redirect_to root_path
+		else
+		flash[:alert] = "There was a problem creating your account. Please try again."
+		redirect_to :back
+		end
+	end
+
+	private
+		def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+		end
+
+end
